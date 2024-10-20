a4a0c86ee9c5f7bb3166710292bc3037276d1fe5
Jane Silver <ivanna.mo091@gmail.com>
2024-10-21 01:53:25 +0300

    Add Home page

---
+++ Frontend/pavuk/src/pages/home/Home.tsx
@@ -0,0 +1,17 @@
+import Navbar from "../../components/home/Navbar";
+import Hero from "../../components/home/Hero";
+import AboutUs from "../../components/home/AboutUs";
+import Footer from "../../components/common/Footer";
+
+function Home() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <AboutUs></AboutUs>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default Home;
