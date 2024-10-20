796a46cce03aa73781593e5cc599a1435e4011be
Jane Silver <ivanna.mo091@gmail.com>
2024-10-20 17:47:22 +0300

    Initial commit

---
+++ Backend/pavuk/src/main/java/com/needleandstitch/pavuk/PavukApplication.java
@@ -0,0 +1,13 @@
+package com.needleandstitch.pavuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PavukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PavukApplication.class, args);
+	}
+
+}
---
+++ Backend/pavuk/src/test/java/com/needleandstitch/pavuk/PavukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.needleandstitch.pavuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PavukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
